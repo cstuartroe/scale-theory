a544e0fb7470662f0639839885f7deaadf62473c
Conor Stuart-Roe <conorstuartroe@gmail.com>
2021-03-15 05:07:29 +0000

    Timer names and percents

--- timer.py
+++ timer.py
@@ -1,8 +1,14 @@
 import time
+from typing import Dict
 from tabulate import tabulate
 
+
 class Timer:
-    def __init__(self):
+    def __init__(self, name=None):
+        self.name = name
+        self.wipe()
+
+    def wipe(self):
         self.task_times = {}
         self.task_iterations = {}
         self.current_task = None
@@ -28,20 +34,33 @@ class Timer:
         self.current_task = None
 
     def log(self, logger=None):
-        s = tabulate(
+        total_time_logged = sum(self.task_times.values())
+        s = f"Timer log: {self.name}\n"
+        s += tabulate(
             [
                 (
                     task_name,
                     self.task_times[task_name],
+                    f"{round(100 * self.task_times[task_name] / total_time_logged, 1)}%",
                     self.task_iterations[task_name],
                     self.task_times[task_name]/self.task_iterations[task_name]
                 )
                 for task_name in self.task_times
             ],
-            ("Task name", "Total time", "Number of iterations", "Average time"),
+            ("Task name", "Total time", "Percent", "Number of iterations", "Average time"),
         )
 
         if logger:
             logger.info(s)
         else:
             print(s)
+
+    @staticmethod
+    def get(name):
+        if name not in TIMERS:
+            TIMERS[name] = Timer(name)
+
+        return TIMERS[name]
+
+
+TIMERS: Dict[str, Timer] = {}
